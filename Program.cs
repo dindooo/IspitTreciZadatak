20050dfbc56235f7d9287b270fa700ae07fdcc0d
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-15 09:27:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspitTreci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int prvi, drugi = 1, sljedeci, brojElemenata;
+            Console.WriteLine("Upisite koliki broj elemenata zelite");
+            brojElemenata = int.Parse(Console.ReadLine());
+            Console.WriteLine("Upisite pocetni broj");
+            prvi = int.Parse(Console.ReadLine());
+            Console.WriteLine(prvi+" "+drugi+" ");
+            for(int i=2; i<brojElemenata; i++)
+            {
+                sljedeci = prvi + drugi;
+                Console.WriteLine(sljedeci + " ");
+                prvi = drugi;
+                drugi = sljedeci;
+            }
+            Console.ReadKey();
+        }
+    }
+}
